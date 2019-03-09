a604f491fc82780f5abff8b875f52d22bdc35e60
Guillaume FERNANDEZ <guillaume@obvy-app.com>
2019-03-09 18:26:49 +0100

    #5 Chat entities

---
+++ Hashgard/Back/Models/ChatMessage.cs
@@ -0,0 +1,23 @@
+﻿using Hashgard.Back.Models.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hashgard.Back.Models
+{
+    public class ChatMessage : Entity
+    {
+        public long CategoryId { get; set; }
+
+        public long UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual IList<ChatMessageView> ChatMessageViewList { get; set; }
+
+        public DateTime SentDateTime { get; set; }
+
+        public string Text { get; set; }
+    }
+}
---
+++ Hashgard/Back/Models/ChatMessageView.cs
@@ -0,0 +1,18 @@
+﻿using Hashgard.Back.Models.Abstract;
+using System;
+
+namespace Hashgard.Back.Models
+{
+    public class ChatMessageView : Entity
+    {
+        public long ChatMessageId { get; set; }
+
+        public virtual ChatMessage ChatMessage { get; set; }
+
+        public long UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public DateTime ViewedDateTime { get; set; }
+    }
+}
